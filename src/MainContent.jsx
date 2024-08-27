305b4207a49d1195a579574f12319c1cef2934fb
Alex_DGL112 <legendgg211@gmail.com>
2024-08-27 12:12:29 +0700

    test_two

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,9 +1,11 @@
+import djmax from "./assets/img/djmax-sobrem.gif"
+
 function MainContent(){
     return(
         <><div className="flex flex-row md:flex items-center justify-center gap-0 h-96 bg-black">
           <div className="bg-black text-white text-center w-screen text-2xl"><h1>Today is friday</h1>
           </div>
-          <div className=" flex text-white justify-center items-center"><img className="w-auto h-[384px]" src="/src/assets/img/djmax-sobrem.gif" alt="carrot carrot" />
+          <div className=" flex text-white justify-center items-center"><img className="w-auto h-[384px]" src={djmax} alt="carrot carrot" />
           </div>
         </div></> 
     )
